a193314f021a835529465e83a3088ec733c56a92
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-02-27 10:31:58 +0000

    hello_world.html template added

--- Blueprints/hello_world.py
+++ Blueprints/hello_world.py
@@ -1,6 +1,6 @@
 # using Blueprint to create a singular page to import into main.py Flask app
 # This can be good for creating your landing page / home page
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 hello_world: Blueprint = Blueprint("hello_world", __name__)
@@ -9,4 +9,4 @@ hello_world: Blueprint = Blueprint("hello_world", __name__)
 @hello_world.route("/")
 @hello_world.route("/hello")
 def hello():
-    return "<h1>Hello World!</h1>"
+    return render_template("hello_world.html")
