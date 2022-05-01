6686aa9f9f58352ee9bdf8ad84340c69c3aeecd6
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-05-01 18:27:20 +0100

    web app connected with frontend

--- rate_my_landlord/project_0.1/app.py
+++ rate_my_landlord/project_0.1/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def landingpage():
-    return "Hello Jasmin!"
+    return render_template("landingPage.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
