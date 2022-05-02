90a8e793b0a7c9927f9bdea50ae58aeaee7d415d
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-05-02 11:44:03 +0100

    homepage added to Python file

--- rate_my_landlord/project_0.1/app.py
+++ rate_my_landlord/project_0.1/app.py
@@ -1,10 +1,14 @@
 from flask import Flask 
-from flask import render_template
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def landingpage():
+    return render_template("landingPage.html")
+
+@app.route("/homepage")
+def homepage():
     return render_template("homepage.html")
 
 if __name__ == "__main__":
