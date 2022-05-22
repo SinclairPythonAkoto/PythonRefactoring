69cd9b0f47acfc3f25a9c570484d6d9486cefd80
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-05-22 02:22:03 +0100

    Adding title to screen, and while loop to prevent game from immediately closing

--- snake_GAME/Step_1/game.py
+++ snake_GAME/Step_1/game.py
@@ -4,6 +4,13 @@ pygame.init()
 
 dis = pygame.display.set_mode((400, 300))
 pygame.display.update()
+pygame.display.set_caption('Snake game by Akoto Tech')
+
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
 
 pygame.quit()
 quit()
