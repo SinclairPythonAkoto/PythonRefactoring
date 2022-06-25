de45482aa6475f32a8a6d89e7068f1d83013747c
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-06-25 23:52:30 +0100

    grouping tests

--- anything_Python/PyTest_Unit_Testing/grouping_tests.py
+++ anything_Python/PyTest_Unit_Testing/grouping_tests.py
@@ -1,5 +1,7 @@
 class TestClass:
-    """How you can group your tests in a class"""
+    """
+        How you can group your tests in a class
+    """
     def test_one(self):
         x = "edureka"
         assert 'r' in x
