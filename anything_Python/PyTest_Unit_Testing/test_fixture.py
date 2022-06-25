8430ddfacb68fabd5ab8bc8ed3666840f65d6377
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-06-26 00:11:35 +0100

    test fixtures

---
+++ anything_Python/PyTest_Unit_Testing/test_fixture.py
@@ -0,0 +1,29 @@
+'''
+Fixures is used in pytest when we need to run a code before the test.
+we use the pytest decorator above the function to set the code we wish 
+to use.
+'''
+
+import pytest
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    c = 25
+    return [a, b, c]
+
+# creating your test case
+def test_method1(numbers):
+    x = 15
+    assert numbers[0] == x
+
+def test_method2(numbers):
+    y = 20
+    assert numbers[1] == y
+
+def test_method3(numbers):
+    x = 25
+    assert numbers[2] == z
+
+
