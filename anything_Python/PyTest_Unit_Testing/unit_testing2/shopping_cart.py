89b77decdc3cb4207bb81140457990ff740f37df
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-06-26 23:05:31 +0100

    blank shopping cart

---
+++ anything_Python/PyTest_Unit_Testing/unit_testing2/shopping_cart.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class ShoppingCart:
+    def __init__(self) -> None:
+        pass
+
+    def add(self, item: str):
+        pass
+
+    def size(self) -> int:
+        return 0
+
+    def fet_items(self) -> List[str]:
+        pass
+
+    def get_total_price(self, price_map):
+        pass
+
