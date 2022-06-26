410fd310df2c4c00da4fbda179ae8020e91b3f82
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-06-27 00:08:10 +0100

    How to test if an error is raised

--- anything_Python/PyTest_Unit_Testing/unit_testing2/test_can_add_item_to_cart.py
+++ anything_Python/PyTest_Unit_Testing/unit_testing2/test_can_add_item_to_cart.py
@@ -1,7 +1,26 @@
 from shopping_cart import ShoppingCart
+import pytest
 
 
-def test_an_add_item_to_cart():
-    cart = ShoppingCart()
+def test_can_add_item_to_cart():
+    cart = ShoppingCart(5)
     cart.add("apple")
     assert cart.size() == 1
+
+
+def test_when_item_added_then_cart_contains_item():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert "apple" in cart.get_items()
+
+
+def test_when_add_more_than_max_items_should_fail():
+    cart = ShoppingCart(5)
+
+    # adding max amount of items
+    for _ in range(5):
+        cart.add("apple")
+    with pytest.raises(OverflowError):
+        # attempting to add over the max amount
+        cart.add("mango")
+
