b6c4bab3af7db0ad2f482bf02d2bcb130f47f7ae
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-06-27 01:01:10 +0100

    Reduced ode smell by implementing pytest fixtures

--- anything_Python/PyTest_Unit_Testing/unit_testing2/test_shopping_cart.py
+++ anything_Python/PyTest_Unit_Testing/unit_testing2/test_shopping_cart.py
@@ -1,32 +1,35 @@
 from shopping_cart import ShoppingCart
 import pytest
 
+# Setting up the max amount of items allowed in basket
+# by using pytest fixtures
+# Good for initialisation.
+@pytest.fixture
+def cart():
+    # all set up for cart here
+    return ShoppingCart(5)
 
 def test_can_add_item_to_cart():
-    cart = ShoppingCart(5)
     cart.add("apple")
     assert cart.size() == 1
 
 
 def test_when_item_added_then_cart_contains_item():
-    cart = ShoppingCart(5)
     cart.add("apple")
     assert "apple" in cart.get_items()
 
 
 def test_when_add_more_than_max_items_should_fail():
-    cart = ShoppingCart(5)
-
     # adding max amount of items
     for _ in range(5):
         cart.add("apple")
+
     with pytest.raises(OverflowError):
         # attempting to add over the max amount
         cart.add("mango")
 
 
 def test_can_get_total_price():
-    cart = ShoppingCart(5)
     cart.add("apple")
     cart.add("orange")
     price_map = {"apple": 1.0, "orange": 2.0}
