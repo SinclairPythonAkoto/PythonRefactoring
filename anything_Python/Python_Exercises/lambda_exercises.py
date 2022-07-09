89c8f8adc822bcd60e06749646cacbc2c817e20f
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-09 03:28:14 +0100

    lambda exercises

---
+++ anything_Python/Python_Exercises/lambda_exercises.py
@@ -0,0 +1,97 @@
+"""Lambda Exercises
+
+Source: https://www.w3resource.com/python-exercises/lambda/index.php
+"""
+
+# 1. Write a Python program to create a lambda function that adds 15 to a given number passed in
+# as an argument, also create a lambda function that multiplies argument x with argument y and
+# print the result.
+# 2. Write a Python program to create a function that takes one argument, and that argument will
+# be multiplied with an unknown given number.
+# 3. Write a Python program to sort a list of tuples using Lambda.
+# Original list of tuples:
+# [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+# Sorting the List of Tuples:
+# [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
+# 4. Write a Python program to sort a list of dictionaries using Lambda.
+# 5. Write a Python program to filter a list of integers using Lambda.
+# 6. Write a Python program to square and cube every number in a given list of integers using Lambda.
+# 7. Write a Python program to find if a given string starts with a given character using Lambda.
+# 8. Write a Python program to extract year, month, date and time using Lambda.
+# 9. Write a Python program to check whether a given string is number or not using Lambda.
+# 10. Write a Python program to create Fibonacci series upto n using Lambda.
+# 11. Write a Python program to find intersection of two given arrays using Lambda.
+# 12. Write a Python program to rearrange positive and negative numbers in a given array using Lambda.
+# 13. Write a Python program to count the even, odd numbers in a given array of integers using Lambda.
+# 14. Write a Python program to find the values of length six in a given list using Lambda.
+# 15. Write a Python program to add two given lists using map and lambda.
+# 16. Write a Python program to find the second lowest grade of any student(s) from the given names and
+# grades of each student using lists and lambda. Input number of students, names and grades of each student.
+# 17. Write a Python program to find numbers divisible by nineteen or thirteen from a list of numbers
+# using Lambda.
+# 18. Write a Python program to find palindromes in a given list of strings using Lambda.
+# 19. Write a Python program to find all anagrams of a string in a given list of strings using lambda.
+# 20. Write a Python program to find the numbers of a given string and store them in a list, display
+# the numbers which are bigger than the length of the list in sorted form. Use lambda function to solve
+# the problem.
+# 21. Write a Python program that multiply each number of given list with a given number using lambda
+# function. Print the result.
+# 22. Write a Python program that sum the length of the names of a given list of names after removing
+# the names that starts with an lowercase letter. Use lambda function.
+# 23. Write a Python program to calculate the sum of the positive and negative numbers of a given
+# list of numbers using lambda function.
+# 24. Write a Python program to find numbers within a given range where every number is divisible
+# by every digit it contains.
+# 25. Write a Python program to create the next bigger number by rearranging the digits of a given number.
+# 26. Write a Python program to find the list with maximum and minimum length using lambda.
+# 27. Write a Python program to sort each sublist of strings in a given list of lists using lambda.
+# 28. Write a Python program to sort a given list of lists by length and value using lambda.
+# 29. Write a Python program to find the maximum value in a given heterogeneous list using lambda.
+# Original list:
+# ['Python', 3, 2, 4, 5, 'version']
+# Maximum values in the said list using lambda:
+# 5
+# 30. Write a Python program to sort a given matrix in ascending order according to the sum of its rows
+# using lambda.
+# 31. Write a Python program to extract specified size of strings from a give list of string values
+# using lambda.
+# 32. Write a Python program to count float number in a given mixed list using lambda.
+# 33. Write a Python program to check whether a given string contains a capital letter, a lower case
+# letter, a number and a minimum length using lambda
+# 34. Write a Python program to filter the height and width of students, which are stored in a
+# dictionary using lambda.
+# 35. Write a Python program to check whether a specified list is sorted or not using lambda.
+# 36. Write a Python program to extract the nth element from a given list of tuples using lambda.
+# 37. Write a Python program to sort a list of lists by a given index of the inner list using lambda.
+# 38. Write a Python program to remove all elements from a given list present in another list using lambda.
+# 39. Write a Python program to find the elements of a given list of strings that contain specific
+# substring using lambda.
+# 40. Write a Python program to find the nested lists elements, which are present in another list
+# using lambda.
+# 41. Write a Python program to reverse strings in a given list of string values using lambda.
+# 42. Write a Python program to calculate the product of a given list of numbers using lambda.
+# 43. Write a Python program to multiply all the numbers in a given list using lambda.
+# 44. Write a Python program to calculate the average value of the numbers in a given tuple of tuples using lambda. Go to the editor
+# Original Tuple:
+# ((10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3))
+# Average value of the numbers of the said tuple of tuples:
+# (30.5, 34.25, 27.0)
+# Original Tuple:
+# ((1, 1, -5), (30, -15, 56), (81, -60, -39), (-10, 2, 3))
+# Average value of the numbers of the said tuple of tuples:
+# (25.5, -18.0, 3.75)
+# 45. Write a Python program to convert string element to integer inside a given tuple using lambda.
+# 46. Write a Python program to find index position and value of the maximum and minimum values in a given
+# list of numbers using lambda.
+# 47. Write a Python program to sort a given mixed list of integers and strings using lambda. Numbers must
+# be sorted before strings.
+# 48. Write a Python program to sort a given list of strings(numbers) numerically using lambda
+# 49. Write a Python program to count the occurrences of the items in a given list using lambda.
+# 50. Write a Python program to remove specific words from a given list using lambda.
+# 51. Write a Python program to find the maximum and minimum values in a given list of tuples using lambda
+# function
+# 52. Write a Python program to remove None value from a given list using lambda function. Go to the editor
+# Original list:
+# [12, 0, None, 23, None, -55, 234, 89, None, 0, 6, -12]
+# Remove None value from the said list:
+# [12, 0, 23, -55, 234, 89, 0, 6, -12]
