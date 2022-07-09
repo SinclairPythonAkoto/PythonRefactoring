f2f649fb9a54e8fd937c4efbd506b508994a9c87
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-09 02:06:10 +0100

    collections exercises

---
+++ anything_Python/Python_Exercises/collections_exercises.py
@@ -0,0 +1,54 @@
+"""Collections Exercises
+
+Source: https://www.w3resource.com/python-exercises/collections/index.php
+"""
+
+# 1. Write a Python program that iterate over elements repeating each as many times as its count.
+# 2. Write a Python program to find the most common elements and their counts of a specified text.
+# 3. Write a Python program to create a new deque with three items and iterate over the deque's elements.
+# 4. Write a Python program to find the occurrences of 10 most common words in a given text.
+# 5. Write a Python program that accept some words and count the number of distinct words. Print the number
+# of distinct words and number of occurrences for each distinct word according to their appearance.
+# 6. Write a Python program that accepts number of subjects, subject names and marks. Input number of
+# subjects and then subject name, marks separated by a space in next line. Print subject name and marks
+# in order of its first occurrence.
+# 7. Write a Python program to create a deque and append few elements to the left and right, then remove
+# some elements from the left, right sides and reverse the deque.
+# 8. Write a Python program to create a deque from an existing iterable object.
+# 9. Write a Python program to add more number of elements to a deque object from an iterable object.
+# 10. Write a Python program to remove all the elements of a given deque object.
+# 11. Write a Python program to copy of a deque object and verify the shallow copying process.
+# 12. Write a Python program to count the number of times a specific element presents in a deque object.
+# 13. Write a Python program to rotate a Deque Object specified number (positive) of times.
+# 14. Write a Python program to rotate a Deque Object specified number (negative) of times.
+# 15. Write a Python program to find the most common element of a given list.
+# 16. Write a Python program to find the second lowest total marks of any student(s) from the given
+# names and marks of each student using lists and lambda. Input number of students, names and grades
+# of each student.
+# 17. Write a Python program to find the majority element from a given array of size n using Collections
+# module.
+# 18. Write a Python program to merge more than one dictionary in a single expression.
+# 19. Write a Python program to break a given list of integers into sets of a given positive number.
+# Return true or false.
+# 20. Write a Python program to find the item with maximum frequency in a given list.
+# 21. Write a Python program to count most and least common characters in a given string.
+# 22. Write a Python program to insert an element at the beginning of a given OrderedDictionary.
+# 23. Write a Python program to get the frequency of the tuples in a given list.
+# 24. Write a Python program to calculate the maximum aggregate from the list of tuples (pairs).
+# 25. Write a Python program to find the characters in a list of strings which occur more than and
+# less than a given number.
+# 26. Write a Python program to find the difference between two list including duplicate elements.
+# Use collections module.
+# 27. Write a Python program to remove duplicate words from a given string use collections module.
+# 28. Write a Python program to create a dictionary grouping a sequence of key-value pairs into a
+# dictionary of lists. Use collections module.
+# 29. Write a Python program to get the frequency of the elements in a given list of lists.
+# Use collections module.
+# 30. Write a Python program to count the occurrence of each element of a given list.
+# 31. Write a Python program to count the most common words in a dictionary.
+# 32. Write a Python program to find the class wise roll number from a tuple-of-tuples.
+# 33. Write a Python program to count the number of students of individual class.
+# 34. Write a Python program to create an instance of an OrderedDict using a given dictionary.
+# Sort the dictionary during the creation and print the members of the dictionary in reverse order.
+# 35. Write a Python program to group a sequence of key-value pairs into a dictionary of lists.
+# 36. Write a Python program to compare two unordered lists (not sets). #
