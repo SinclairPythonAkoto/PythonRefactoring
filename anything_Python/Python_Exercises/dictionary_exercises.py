52d02911fcb5844cbc4045bc90a2b81d8f709403
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-09 05:39:35 +0100

    dictionary exercises

---
+++ anything_Python/Python_Exercises/dictionary_exercises.py
@@ -0,0 +1,94 @@
+"""Dictionary Exercises
+
+Source: https://www.w3resource.com/python-exercises/dictionary/
+"""
+
+# 1. Write a Python script to sort (ascending and descending) a dictionary by value.
+# 2. Write a Python script to add a key to a dictionary.
+# 3. Write a Python script to concatenate following dictionaries to create a new one.
+# 4. Write a Python script to check whether a given key already exists in a dictionary.
+# 5. Write a Python program to iterate over dictionaries using for loops.
+# 6. Write a Python script to generate and print a dictionary that contains a number
+# (between 1 and n) in the form (x, x*x).
+# 7. Write a Python script to print a dictionary where the keys are numbers between 1 and
+# 15 (both included) and the values are square of keys.
+# 8. Write a Python script to merge two Python dictionaries.
+# 9. Write a Python program to iterate over dictionaries using for loops.
+# 10. Write a Python program to sum all the items in a dictionary.
+# 11. Write a Python program to multiply all the items in a dictionary.
+# 12. Write a Python program to remove a key from a dictionary.
+# 13. Write a Python program to map two lists into a dictionary.
+# 14. Write a Python program to sort a given dictionary by key.
+# 15. Write a Python program to get the maximum and minimum value in a dictionary.
+# 16. Write a Python program to get a dictionary from an object's fields.
+# 17. Write a Python program to remove duplicates from Dictionary.
+# 18. Write a Python program to check a dictionary is empty or not.
+# 19. Write a Python program to combine two dictionary adding values for common keys.
+# 20. Write a Python program to print all unique values in a dictionary.
+# 21. Write a Python program to create and display all combinations of letters, selecting each
+# letter from a different key in a dictionary.
+# 22. Write a Python program to find the highest 3 values of corresponding keys in a dictionary.
+# 23. Write a Python program to combine values in python list of dictionaries.
+# 24. Write a Python program to create a dictionary from a string.
+# 25. Write a Python program to print a dictionary in table format.
+# 26. Write a Python program to count the values associated with key in a dictionary.
+# 27. Write a Python program to convert a list into a nested dictionary of keys.
+# 28. Write a Python program to sort a list alphabetically in a dictionary.
+# 29. Write a Python program to remove spaces from dictionary keys.
+# 30. Write a Python program to get the top three items in a shop.
+# 31. Write a Python program to get the key, value and item in a dictionary.
+# 32. Write a Python program to print a dictionary line by line.
+# 33. Write a Python program to check multiple keys exists in a dictionary.
+# 34. Write a Python program to count number of items in a dictionary value that is a list.
+# 35. Write a Python program to sort Counter by value.
+# 36. Write a Python program to create a dictionary from two lists without losing duplicate values.
+# 37. Write a Python program to replace dictionary values with their average.
+# 38. Write a Python program to match key values in two dictionaries.
+# 39. Write a Python program to store a given dictionary in a json file.
+# 40. Write a Python program to create a dictionary of keys x, y, and z where each key has as value
+# a list from 11-20, 21-30, and 31-40 respectively. Access the fifth value of each key from the dictionary.
+# 41. Write a Python program to drop empty Items from a given Dictionary.
+# 42. Write a Python program to filter a dictionary based on values.
+# 43. Write a Python program to convert more than one list to nested dictionary.
+# 44. Write a Python program to filter the height and width of students, which are stored in a dictionary.
+# 45. Write a Python program to check all values are same in a dictionary.
+# 46. Write a Python program to create a dictionary grouping a sequence of key-value pairs into a dictionary of lists.
+# 47. Write a Python program to split a given dictionary of lists into list of dictionaries.
+# 48. Write a Python program to remove a specified dictionary from a given list.
+# 49. Write a Python program to convert string values of a given dictionary, into integer/float datatypes.
+# 50. A Python Dictionary contains List as value. Write a Python program to clear the list values in the said dictionary.
+# 51. A Python Dictionary contains List as value. Write a Python program to update the list values in the said dictionary.
+# 52. Write a Python program to extract a list of values from a given list of dictionaries.
+# 53. Write a Python program to find the length of a given dictionary values.
+# 54. Write a Python program to get the depth of a dictionary.
+# 55. Write a Python program to access dictionary key's element by index.
+# 56. Write a Python program to convert a given dictionary into a list of lists.
+# 57. Write a Python program to filter even numbers from a given dictionary values.
+# 58. Write a Python program to get all combinations of key-value pairs in a given dictionary.
+# 59. Write a Python program to find the specified number of maximum values in a given dictionary.
+# 60. Write a Python program to find shortest list of values with the keys in a given dictionary.
+# 61. Write a Python program to count the frequency in a given dictionary.
+# 62. Write a Python program to extract values from a given dictionaries and create a list of lists from those values.
+# 63. Write a Python program to convert a given list of lists to a dictionary.
+# 64. Write a Python program to create a key-value list pairings in a given dictionary.
+# 65. Write a Python program to get the total length of all values of a given dictionary with string values.
+# 66. Write a Python program to check if a specific Key and a value exist in a dictionary.
+# 67. Write a Python program to invert a given dictionary with non-unique hashable values.
+# 68. Write a Python program to combines two or more dictionaries, creating a list of values for each key.
+# 69. Write a Python program to group the elements of a given list based on the given function.
+# 70. Write a Python program to map the values of a given list to a dictionary using a function, where the
+# key-value pairs consist of the original value as the key and the result of the function as the value.
+# 71. Write a Python program to retrieve the value of the nested key indicated by the given selector list from
+# a dictionary or list.
+# 72. Write a Python program to invert a dictionary with unique hashable values.
+# 73. Write a Python program to convert a list of dictionaries into a list of values corresponding to the specified key.
+# 74. Write a Python program to create a dictionary with the same keys as the given dictionary and values
+# generated by running the given function for each value.
+# 75. Write a Python program to find all keys in the provided dictionary that have the given value.
+# 76. Write a Python program to combine two lists into a dictionary, where the elements of the first one serve
+# as the keys and the elements of the second one serve as the values. The values of the first list need to be unique
+# and hashable.
+# 77. Write a Python program to convert given a dictionary to a list of tuples.
+# 78. Write a Python program to create a flat list of all the keys in a flat dictionary.
+# 79. Write a Python program to create a flat list of all the values in a flat dictionary.
+# 80. Write a Python program to find the key of the maximum value in a dictionary. #
