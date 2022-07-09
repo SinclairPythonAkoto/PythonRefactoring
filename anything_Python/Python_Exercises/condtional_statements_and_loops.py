43d481a15d3949812e17c431a09f3fabf3e8e048
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-09 02:30:47 +0100

    exercises on conditional statements and loops

---
+++ anything_Python/Python_Exercises/condtional_statements_and_loops.py
@@ -0,0 +1,63 @@
+"""Conditional Statements & Loops
+
+Source: https://www.w3resource.com/python-exercises/python-conditional-statements-and-loop-exercises.php
+"""
+
+# 1. Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500
+# and 2700 (both included).
+# 2. Write a Python program to convert temperatures to and from celsius, fahrenheit.
+# [ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ]
+# 3. Write a Python program to guess a number between 1 to 9.
+# 4. Write a Python program to construct the following pattern, using a nested for loop.
+# 5. Write a Python program that accepts a word from the user and reverse it.
+# 6. Write a Python program to count the number of even and odd numbers from a series of numbers.
+# 7. Write a Python program that prints each item and its corresponding type from the following list.
+# 8. Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+# Note : Use 'continue' statement.
+# 9. Write a Python program to get the Fibonacci series between 0 to 50.
+# Note : The Fibonacci Sequence is the series of numbers : 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+# 10. Write a Python program which iterates the integers from 1 to 50. For multiples of three print
+# "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are
+# multiples of both three and five print "FizzBuzz".
+# 11. Write a Python program which takes two digits m (row) and n (column) as input and generates a
+# two-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+# 12. Write a Python program that accepts a sequence of lines (blank line to terminate) as input and
+# prints the lines as output (all characters in lower case).
+# 13. Write a Python program which accepts a sequence of comma separated 4 digit binary numbers as
+# its input and print the numbers that are divisible by 5 in a comma separated sequence.
+# 14. Write a Python program that accepts a string and calculate the number of digits and letters.
+# 15. Write a Python program to check the validity of password input by users.
+# 16. Write a Python program to find numbers between 100 and 400 (both included) where each digit
+# of a number is an even number. The numbers obtained should be printed in a comma-separated sequence.
+# 17. Write a Python program to print alphabet pattern 'A'.
+# 18. Write a Python program to print alphabet pattern 'D'.
+# 19. Write a Python program to print alphabet pattern 'E'.
+# 20. Write a Python program to print alphabet pattern 'G'.
+# 21. Write a Python program to print alphabet pattern 'L'.
+# 22. Write a Python program to print alphabet pattern 'M'.
+# 23. Write a Python program to print alphabet pattern 'O'.
+# 24. Write a Python program to print alphabet pattern 'P'.
+# 25. Write a Python program to print alphabet pattern 'R'.
+# 26. Write a Python program to print the following patterns.
+# 27. Write a Python program to print alphabet pattern 'T'.
+# 28. Write a Python program to print alphabet pattern 'U'.
+# 29. Write a Python program to print alphabet pattern 'X'.
+# 30. Write a Python program to print alphabet pattern 'Z'.
+# 31. Write a Python program to calculate a dog's age in dog's years.
+# 32. Write a Python program to check whether an alphabet is a vowel or consonant.
+# 33. Write a Python program to convert month name to a number of days.
+# 34. Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it
+# will return 20.
+# 35. Write a Python program to check a string represent an integer or not.
+# 36. Write a Python program to check a triangle is equilateral, isosceles or scalene.
+# 37. Write a Python program that reads two integers representing a month and day and prints the season
+# for that month and day.
+# 38. Write a Python program to display astrological sign for given date of birth.
+# 39. Write a Python program to display the sign of the Chinese Zodiac for given year in which you were
+# born.
+# 40. Write a Python program to find the median of three values.
+# 41. Write a Python program to get next day of a given date.
+# 42. Write a Python program to calculate the sum and average of n integer numbers (input from the user).
+# Input 0 to finish.
+# 43. Write a Python program to create the multiplication table (from 1 to 10) of a number.
+# 44. Write a Python program to construct the following pattern, using a nested loop number.
