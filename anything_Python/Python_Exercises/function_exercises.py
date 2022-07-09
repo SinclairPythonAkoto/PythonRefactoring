33b2c0dec32c5438f9ef870a46fdc0abcac16f65
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-09 02:41:53 +0100

    function exercises

---
+++ anything_Python/Python_Exercises/function_exercises.py
@@ -0,0 +1,32 @@
+"""Function Exercises
+
+Source: https://www.w3resource.com/python-exercises/python-functions-exercises.php
+"""
+
+# 1. Write a Python function to find the Max of three numbers.
+# 2. Write a Python function to sum all the numbers in a list.
+# 3. Write a Python function to multiply all the numbers in a list.
+# 4. Write a Python program to reverse a string.
+# 5. Write a Python function to calculate the factorial of a number (a non-negative integer).
+# The function accepts the number as an argument.
+# 6. Write a Python function to check whether a number falls in a given range.
+# 7. Write a Python function that accepts a string and calculate the number of upper case
+# letters and lower case letters.
+# 8. Write a Python function that takes a list and returns a new list with unique elements of
+# the first list.
+# 9. Write a Python function that takes a number as a parameter and check the number is prime or not.
+# 10. Write a Python program to print the even numbers from a given list.
+# 11. Write a Python function to check whether a number is perfect or not.
+# 12. Write a Python function that checks whether a passed string is palindrome or not.
+# 13. Write a Python function that prints out the first n rows of Pascal's triangle.
+# 14. Write a Python function to check whether a string is a pangram or not.
+# 15. Write a Python program that accepts a hyphen-separated sequence of words as input and prints
+# the words in a hyphen-separated sequence after sorting them alphabetically.
+# 16. Write a Python function to create and print a list where the values are square of numbers
+# between 1 and 30 (both included).
+# 17. Write a Python program to make a chain of function decorators (bold, italic, underline etc.)
+# in Python.
+# 18. Write a Python program to execute a string containing Python code.
+# 19. Write a Python program to access a function inside a function.
+# 20. Write a Python program to detect the number of local variables declared in a function.
+# 21. Write a Python program that invoke a given function after specific milliseconds.
