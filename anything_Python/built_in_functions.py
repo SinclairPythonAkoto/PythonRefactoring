f1a131c9d623c0bfea453f668d2aeb1a8bb30a3b
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-21 15:40:35 +0100

    Built-in functions: all, any, sorted, max, min, reversed, abs, sum, round

--- anything_Python/built_in_functions.py
+++ anything_Python/built_in_functions.py
@@ -168,8 +168,3 @@ def extremes(element):
 print(extremes([1, 2, 3, 4, 5]))    # (1, 5)
 print(extremes((99, 25, 30, -7)))   # (-7, 99)
 print(extremes("alcatraz"))         # ('a', 'z')
-
-
-"""
-Abs, Sum, Round Function
-"""
