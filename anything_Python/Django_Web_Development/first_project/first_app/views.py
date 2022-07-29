75715afff4205894c77f567d908747bb681cde08
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-29 23:27:02 +0100

    dynamic route added

--- anything_Python/Django_Web_Development/first_project/first_app/views.py
+++ anything_Python/Django_Web_Development/first_project/first_app/views.py
@@ -12,3 +12,8 @@ def home(request):
 
 def educative(request):
     return HttpResponse("Welcome to the Eductaive page!")
+
+
+def show_age(request, age):
+    return HttpResponse("I am %s years old" % age)
+    # return HttpResponse(f"I am {age} years old") # f-string can also be used
--- anything_Python/Django_Web_Development/first_project/first_project/urls.py
+++ anything_Python/Django_Web_Development/first_project/first_project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("educative/", views.educative, name="educative"),
+    path("<age>/", views.show_age, name="show_age"),
     path("admin/", admin.site.urls),
 ]
