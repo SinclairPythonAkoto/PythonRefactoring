e2883af6ebd658d7afbcfcd42055ac313d840ee5
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-29 23:22:05 +0100

    static route created

--- anything_Python/Django_Web_Development/first_project/first_app/views.py
+++ anything_Python/Django_Web_Development/first_project/first_app/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
+
+def home(request):
+    return HttpResponse("Welcome to the home page!")
+
+
+def educative(request):
+    return HttpResponse("Welcome to the Eductaive page!")
--- anything_Python/Django_Web_Development/first_project/first_project/urls.py
+++ anything_Python/Django_Web_Development/first_project/first_project/urls.py
@@ -19,5 +19,7 @@ from first_app import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("home/", views.home, name="home"),
+    path("educative/", views.educative, name="educative"),
     path("admin/", admin.site.urls),
 ]
