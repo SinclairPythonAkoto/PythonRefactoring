80f9d9c7a95b0f587da3bf554a6fb6d029f9528a
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-30 00:46:08 +0100

    path converters added

--- anything_Python/Django_Web_Development/first_project/first_app/views.py
+++ anything_Python/Django_Web_Development/first_project/first_app/views.py
@@ -14,6 +14,14 @@ def educative(request):
     return HttpResponse("Welcome to the Eductaive page!")
 
 
-def show_age(request, age):
-    return HttpResponse("I am %s years old" % age)
-    # return HttpResponse(f"I am {age} years old") # f-string can also be used
+# def show_age(request, age):
+#     return HttpResponse("I am %s years old" % age)
+# return HttpResponse(f"I am {age} years old") # f-string can also be used
+
+
+def even_or_odd(request, num):
+    if num % 2 == 0:
+        output = "%s is an even number." % num
+    else:
+        output = "%s is an odd number." % num
+    return HttpResponse(output)
--- anything_Python/Django_Web_Development/first_project/first_project/urls.py
+++ anything_Python/Django_Web_Development/first_project/first_project/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("educative/", views.educative, name="educative"),
-    path("<age>/", views.show_age, name="show_age"),
+    path("<int:num>/", views.even_or_odd, name="even_or_odd"),
     path("admin/", admin.site.urls),
 ]
