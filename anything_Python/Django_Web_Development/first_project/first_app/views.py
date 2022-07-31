fb9e459ef4aa953f343f899f8c9bdfceb6636d86
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-07-31 18:02:02 +0100

    basic form validation

--- anything_Python/Django_Web_Development/first_project/first_app/views.py
+++ anything_Python/Django_Web_Development/first_project/first_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 from .forms import TestForm
